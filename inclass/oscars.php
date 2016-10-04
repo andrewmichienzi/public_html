1c023b64c9a34eaaa8e6be9fb4bcf2857a4449d5
michiena <michiena@mail.gvsu.edu>
2016-10-04 15:52:14 -0400

    Creating assignment4 directory

--- inclass/oscars.php
+++ inclass/oscars.php
@@ -3,13 +3,23 @@
 <head>
 	<title>inclass1</title>
 <body>
-<p>hello
+<p>
 <?php
 $filename = "oscars.txt";
 $contents = file($filename);
 
+$headings = ["Best Actor", "Best Picture", "Best Actress"];
 foreach($contents as $line) {
-    echo $line . "<br>";
+	$line = trim($line);
+	if(array_search($line, $heading)){
+		echo "hello there";
+		echo "<h3>". $line. "</h3>";
+	}
+	else if($line== null)
+		echo "<br>";
+	else
+		echo "<p>".$line."</p>";
+
 }
 ?>
 </p>
