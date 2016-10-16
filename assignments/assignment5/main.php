c204431717b429f88a4ae2db0138953535262ef0
michiena <michiena@mail.gvsu.edu>
2016-10-16 12:15:37 -0400

    Assignment 5: Able to read from customer.txt and insert all customers into the database

--- assignments/assignment5/main.php
+++ assignments/assignment5/main.php
@@ -1,13 +1,10 @@
 <!DOCTYPE>
 <html>
 <head>
-	<title>assignmnet 5 php</title>
+	<title>Assignment5</title>
 </head>
 <body>
-	<p>
 		
-	</p>
-</head>
 
 <?php
 $servername = "localhost";
@@ -15,26 +12,69 @@ $username = "michiena";
 $password = "michiena6390";
 $dbname = "michiena";
 
+
 echo "hello there";
 $conn = mysql_connect($servername, $username, $password);
 if($conn->connection_error)
 	die("Connection failed: " . $conn->connect_error);
-else
-	echo "connection worked";
-
-echo "hey";	
-$sql = "SELECT * FROM Customer;";
 
 mysql_select_db('michiena');
-$retval = mysql_query($sql, $conn);
-if(! $retval )
-{
-	die('could not get data ' . mysql_error());
+//createTable($conn);
+insertTextField($conn);
+/*
+$sql = "Select * from Customer;";
+	$retval = mysql_query($sql, $conn);
+	if(! $retval )
+	{
+		die('could not get data ' . mysql_error());
+	}
+	
+	//return $retval;
+	//Read Result
+	while($row = mysql_fetch_array($retval, MYSQL_ASSOC))
+	{
+		echo "Customer Name :{$row['firstName']} <br> ";
+	}
+	echo"end";
+
+*/
+function insertTextField($conn){
+	echo "insert Text Field<br>";
+
+	$filename = "customers.txt";
+	$contents = file($filename);
+	echo $contents[1];
+	$lineNumber = 0;
+	$delim = ",";
+	foreach ($contents as $line)
+	{
+		//echo "<p>".$line."</p>";
+		if($lineNumber == 0)
+		{
+			$lineNumber++;
+			continue;
+		}
+		$pieces = explode($delim, $line);
+		
+		$id = $pieces[0];
+		$firstName = $pieces[1];
+		$lastName = $pieces[2];
+		$address = $pieces[3];
+		$city = $pieces[4];
+		$state = $pieces[5];
+		$zip = $pieces[6];
+		$creditCard = $pieces[7];
+		$balance = $pieces[8];
+
+		$sql = "INSERT INTO Customer (id, firstName, lastName, address, city, state, zip, creditCard, balance) VALUES ('".$id."', '".$firstName."', '".$lastName."', '".$address."', '".$city."', '".$state."', '".$zip."', '".$creditCard."', '".$balance."');";
+		$retVal = mysql_query($sql, $conn);
+		if(! $retVal){
+			die('Could not get data ' . mysql_error()); 
+		}
+		$lineNumber++;
+	} 	
 }
-while($row = mysql_fetch_array($retval, MYSQL_ASSOC))
-{
-	echo "Customer Name :{$row['firstName']} <br> ";
-}
-echo"end";
-$conn->close();	
+
 ?>
+</body>
+</html>
